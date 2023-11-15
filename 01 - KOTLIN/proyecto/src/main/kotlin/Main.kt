899be2c6588e491dc4099c74961fe45bc52c4947
Ricardo Campaña <gary.campana@epn.edu.ec>
2023-11-15 07:51:02 -0500

    Hasta variables

--- 01 - KOTLIN/proyecto/src/main/kotlin/Main.kt
+++ 01 - KOTLIN/proyecto/src/main/kotlin/Main.kt
@@ -1,7 +1,27 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import sun.font.TrueTypeFont
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    println("Hello World!")
+    // INMUTABLES (NO se reasignan "=")
+    val inmutable:string ="Adrian";
+    // Inmutable ="Vicente";
+
+    // Mutable (Re asignar)
+    var mutable:String ="Vicente";
+    mutable ="Adrian";
+
+    // val > var
+    // Duck Typing
+    var ejemploVariable ="Gary Campaña"
+    val edadEjejmplo: Int = 12
+    ejemploVariable.trim()
+    // ejemploVariable =edadEjemplo
+
+    //variable primitiva
+    val nombreProfesor:String ="Adrian Eguez"
+    val sueldo:Double = 1.2
+    val estadoCivil: Char = 'C'
+    val mayorEdad: Boolean = True
+    //Clases Java
+    val fechaNacimiento: Date = Date()
 }
