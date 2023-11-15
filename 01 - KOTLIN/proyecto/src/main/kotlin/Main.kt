12a430341f17eb3d490ed20af440af2d8aca0040
Ricardo Campaña <gary.campana@epn.edu.ec>
2023-11-15 09:56:39 -0500

    Hasta la clase 04

--- 01 - KOTLIN/proyecto/src/main/kotlin/Main.kt
+++ 01 - KOTLIN/proyecto/src/main/kotlin/Main.kt
@@ -46,6 +46,10 @@ fun main() {
     calcularSueldo(10.00,15.00,20.00)
     calcularSueldo(10.00, bonoEspecial = 20.00) //Named Parameters
     calcularSueldo(bonoEspecial = 20.00, sueldo = 10.00,tasa=14.00)
+
+    val sumaUno = Suma(1,1)
+    val sumaDos = Suma (null,1)
+    val sumaTres = Suma(1, null)
 }
 abstract class NumerosJava{
     protected val numeroUno: Int
@@ -59,6 +63,52 @@ abstract class NumerosJava{
         println("Inicializando")
     }
 }
+
+abstract class Numeros(
+    //Ejemplo:
+    // uno: Int, (Parametro (Sin modificador de acceso))
+    // private var uno: Int, //Propiedad publica Clase numero uno
+    // var uno: Int //Propiedad de la clase (por defecto es PUBLIC)
+    //public var uno: Int,
+    protected val numeroUno: Int, //Propiedad de la clase protected numeros.numeroUno
+    protected val numeroDos: Int, //Propiedad de la clase protected numeros.numeroDos
+){
+    //var cedula:string ="" (Public es por defecto)
+    //private valorCalculado: Int = 0 (private)
+
+    init{ //Bloque constructor primario
+        this.numeroUno; this.numeroDos //this es opcional
+        numeroUno; numeroDos //Sin el this, es lo mismo
+        println("Inicializando")
+    }
+}
+
+
+class Suma( //Constructor Primario Suma
+    unoParametro:Int, //Parametro
+    dosParametro: Int, //Parametro
+): Numeros(unoParametro, dosParametro){ //Extendido y mandando los parametros (suoer)
+    init{ //Bloque codigo constructor primario
+        this.numeroUno
+        this.numeroDos
+    }
+
+    constructor( //Segundo Constructor
+        uno: Int?, //Parametros
+        dos:Int//Parametros
+    ):this(
+        if(uno==null)0 else uno, dos
+    )
+
+    constructor( //Tercer Constructor
+        uno: Int, //Parametros
+        dos:Int? //Parametros
+    ):this(
+        uno,
+        if(dos==null)0 else dos
+    )
+}
+
 //Void -> Unit
 fun imprimirNombre(nombre:String):Unit{
     //"Nombre: " + nombre
