8c2c24b37d98d5c93bc5a682e015bd144fe6bb50
Ricardo Campaña <gary.campana@epn.edu.ec>
2023-11-15 08:27:10 -0500

    Hasta Funciones

--- 01 - KOTLIN/proyecto/src/main/kotlin/Main.kt
+++ 01 - KOTLIN/proyecto/src/main/kotlin/Main.kt
@@ -24,4 +24,41 @@ fun main() {
     val mayorEdad: Boolean = True
     //Clases Java
     val fechaNacimiento: Date = Date()
+
+    // SWITCH
+    val estadoCivilWhen ="C"
+    when (estadoCivilWhen){
+        ("C") -> {
+            println("Casado")
+        }
+        "S" ->{
+            println("Soltero")
+        }
+        else ->{
+            println("No sabemos")
+        }
+    }
+
+    val esSoltero = (estadoCivilWhen =="S")
+    val coqueto = if (esSoltero) "Si" else "No"
+}
+
+//Void -> Unit
+fun imprimirNombre(nombre:String):Unit{
+    //"Nombre: " + nombre
+    println ("Nombre: ${nombre}")
+}
+
+fun calcularSueldo(
+    sueldo: Double,
+    tasa:Double=12.00,
+    bonoEspecial:Double?=null,):Double{
+    // Int -> Int? (nulleable)
+    // String -> String? (nulleable)
+    // Date -> Date? (nulleable)
+    if(bonoEspecial ==null){
+        return sueldo*(100/tasa)
+    }else{
+        return sueldo * (100/tasa)+ bonoEspecial
+    }
 }
