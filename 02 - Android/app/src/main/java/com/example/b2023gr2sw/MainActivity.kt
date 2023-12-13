0db1164f68c863846ddfc9c1646d3c233b3a1ebd
RicardoCR98 <gary.campana@epn.edu.ec>
2023-12-12 23:25:23 -0500

    Hasta clase 12

--- 02 - Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
+++ 02 - Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
@@ -1,13 +1,31 @@
 package com.example.b2023gr2sw
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val botonCicloVida = findViewById<Button>(R.id.btn_ciclo_vida)
+        botonCicloVida
+            .setOnClickListener {
+                irActividad(ACicloVida::class.java)
+            }
+
+        val botonListView = findViewById<Button>(R.id.btn_ir_list_view)
+        botonListView
+            .setOnClickListener {
+                irActividad(BListView::class.java)
+            }
+
+    }
+    fun irActividad(
+        clase: Class<*>
+    ){
+        val intent = Intent(this, clase)
+        startActivity(intent)
     }
 }
-
