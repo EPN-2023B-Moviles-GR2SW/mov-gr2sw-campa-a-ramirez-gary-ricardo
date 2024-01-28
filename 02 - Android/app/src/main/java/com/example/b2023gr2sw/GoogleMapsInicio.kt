ee18806f90c8c3f6b5caa89454f99b35f2385774
RicardoCR98 <gary.campana@epn.edu.ec>
2024-01-27 22:47:31 -0500

    Hasta servicios de google

---
+++ 02 - Android/app/src/main/java/com/example/b2023gr2sw/GoogleMapsInicio.kt
@@ -0,0 +1,11 @@
+package com.example.b2023gr2sw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GoogleMapsInicio : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_google_maps_inicio)
+    }
+}
